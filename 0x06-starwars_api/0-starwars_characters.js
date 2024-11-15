9f275644ecdfd3ce88f24b91ea186e95dd1d79c1
2005-annabel <owangeshi@gmail.com>
2024-11-15 08:23:16 +0300

    starwars_api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -8,32 +8,32 @@ let people = [];
 const names = [];
 
 const requestCharacters = async () => {
-  await new Promise(resolve => {
-    request(filmEndPoint, (err, res, body) => {
-      if (err || res.statusCode !== 200) {
-        console.error('Error fetching film data:', err || res.statusCode);
-      } else {
-        const jsonBody = JSON.parse(body);
-        people = jsonBody.characters;
-      }
+  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
+    if (err || res.statusCode !== 200) {
+      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+    } else {
+      const jsonBody = JSON.parse(body);
+      people = jsonBody.characters;
       resolve();
-    });
-  });
+    }
+  }));
 };
 
 const requestNames = async () => {
-  for (const p of people) {
-    await new Promise(resolve => {
-      request(p, (err, res, body) => {
+  if (people.length > 0) {
+    for (const p of people) {
+      await new Promise(resolve => request(p, (err, res, body) => {
         if (err || res.statusCode !== 200) {
-          console.error('Error fetching character data:', err || res.statusCode);
+          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
         } else {
           const jsonBody = JSON.parse(body);
           names.push(jsonBody.name);
+          resolve();
         }
-        resolve();
-      });
-    });
+      }));
+    }
+  } else {
+    console.error('Error: Got no Characters for some reason');
   }
 };
 
@@ -41,8 +41,12 @@ const getCharNames = async () => {
   await requestCharacters();
   await requestNames();
 
-  for (let i = 0; i < names.length; i++) {
-    process.stdout.write(names[i] + (i < names.length - 1 ? '\n' : ''));
+  for (const n of names) {
+    if (n === names[names.length - 1]) {
+      process.stdout.write(n);
+    } else {
+      process.stdout.write(n + '\n');
+    }
   }
 };
 
